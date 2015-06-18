60fce27818c03cde7b12cff0af118bd468df0913
Nottapesahc <Nottapesahc@gmail.com>
2015-06-17 20:14:50 -0400

    The build method now steps through the incoming lines and breaks them into their appropriate pieces, and assigns them to a node.

--- AVLTree.py
+++ AVLTree.py
@@ -1,9 +1,8 @@
 __author__ = 'stevenpatton'
 
-
 class Node:
     def __init__(self):
-        self.id = "abc"
+        self.id = ''
         self.data1 = 0
         self.data2 = 0
         self.parent = None
@@ -16,14 +15,30 @@ class Node:
 
 class AVLTree:
     def __init__(self):
-        self.root = None
-        self.current = None
+        self.root = ''
+        self.current = ''
 
     def build(self, filename):
         file = open(filename, 'r')
         for line in file:
-            print(line)
-        file.close()
+            # New node for each line
+            node = Node()
+            # break up the line and assign the id, data1 and data2 to the new node
+            place_holder = 0
+            for i in range(len(line)):
+                if line[i] == ' ' and node.id == '':
+                    node.id = line[0:i]
+                    place_holder = i + 1
 
+                if line[i] == ' ' and not (node.id == ''):
+                    node.data1 = line[place_holder:i]
+                    node.data2 = line[i + 1:len(line) - 1]
+
+            # set first node to the root
+            if self.root == '':
+                self.root = node
+
+        file.close()
+# run
 a = AVLTree()
-a.build("data.txt")
+a.build("SuperImportantData.txt")
