3891ea013bb86b4e9dff8f44f225cfcfd73d8af0
Nottapesahc <Nottapesahc@gmail.com>
2015-06-22 20:40:06 -0400

    Removed the iterative insert, and replaced with a recursive implementation

--- AVLTree.py
+++ AVLTree.py
@@ -2,9 +2,9 @@ __author__ = 'stevenpatton'
 
 class Node:
     def __init__(self):
-        self.id = ''
-        self.data1 = 0
-        self.data2 = 0
+        self.id = 0
+        self.data1 = ''
+        self.data2 = ''
         self.parent = None
         self.leftChild = None
         self.rightChild = None
@@ -16,53 +16,43 @@ class Node:
 class AVLTree:
     def __init__(self):
         self.root = None
-        self.current = self.root
 
     def build(self, filename):
         file = open(filename, 'r')
+        a = 0
         for line in file:
             # New node for each line
             node = Node()
             # break up the line and assign the id, data1 and data2 to the new node
             place_holder = 0
             for i in range(len(line)):
-                if line[i] == ' ' and node.id == '':
+                if line[i] == ' ' and node.id == 0:
                     node.id = line[0:i]
                     place_holder = i + 1
-                if line[i] == ' ' and not (node.id == ''):
+                if line[i] == ' ' and not (node.id == 0):
                     node.data1 = line[place_holder:i]
                     node.data2 = line[i + 1:len(line) - 1]
 
-            a = 1
-            self.insert(node)
+            if not(node.id == 0):
+                self.insert(node, self.root)
+                a += 1
         file.close()
 
-    def insert(self, node):
-        self.current = self.root
-        while True:
-            # First node will be set to root
-            if self.root is None:
-                self.root = node
-                break
+    def insert(self, new_node, current):
+        if self.root is None:
+            self.root = new_node
+            return
+        if not(current.leftChild is None) and current.id > new_node.id:
+            return self.insert(new_node, current.leftChild)
+        if not(current.rightChild is None) and current.id < new_node.id:
+            return self.insert(new_node, current.rightChild)
+        if current.id > new_node.id:
+            current.leftChild = new_node
+            new_node.parent = current
+        if current.id < new_node.id:
+            current.rightChild = new_node
+            new_node.parent = current
 
-            # null condition left
-            if self.current.id > node.id and self.current.leftChild is None:
-                self.current.leftChild = node
-                node.parent = self.current
-                break
-
-            # null condition right
-            if self.current.id < node.id and self.current.rightChild is None:
-                self.current.rightChild = node
-                node.parent = self.current
-                break
-
-            # traversal cases
-            else:
-                if self.current.id < node.id:
-                    self.current = self.current.rightChild
-                if self.current.id > node.id:
-                    self.current = self.current.leftChild
 
 
 
